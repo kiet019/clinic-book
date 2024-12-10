45a12b8d1fb1747535e52f1f60779ce11121ea9a
Hashiramanh <vanmanh23012003@gmail.com>
2024-12-10 22:09:13 +0700

    update ui admin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Các import cho các trang
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,8 +17,6 @@ import VoiceCall from "./pages/voiceCall";
 import Favourites from "./pages/Favourites";
 import Booking from "./pages/Booking";
 import Chat from "./pages/Chat";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import ScheduleTimings from "./pages/scheduleTimings";
 import DoctorRegister from "./pages/doctorRegister";
 import Invoice from "./pages/Invoice";
@@ -29,13 +32,83 @@ import InvoiceView from "./pages/invoiceView";
 import ProfileSettings from "./pages/profileSettings";
 import ChangePassword from "./pages/changePassword";
 import ForgotPassword from "./pages/forgotPassword";
-import patientProfile from "./pages/patientProfile";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { SnackbarProvider } from "notistack";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UserDashboard from "./pages/UserDashboard";
+
+// Import Header, Footer
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Các import cho Admin
+import AdminLayout from './components/AdminLayout'; 
+import AdminDashboard from './pages/admin/AdminDashboard';
+import Specialities from './pages/admin/Specialities';
+import DoctorList from './pages/admin/DoctorList';
+import PatientList from './pages/admin/PatientList';
+import TransactionsList from './pages/admin/TransactionsList';
+import Appointment from './pages/admin/Appointment';
+
+// Hook để kiểm tra route hiện tại
+function Layout() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  return (
+    <div>
+      {/* Chỉ hiển thị Header và Footer nếu không phải route admin */}
+      {!isAdminRoute && <Header />}
+      
+      <Routes>
+        {/* Các route cho trang chính */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/doctorProfile" element={<DoctorProfile />} />
+        <Route path="/doctorAppointment" element={<DoctorProfile />} />
+        <Route path="/patientProfile" element={<PatientProfile />} />
+        <Route path="/patientDashboard" element={<PatientProfile />} />
+        <Route path="/profileSettings" element={<ProfileSettings />} />
+        <Route path="/Reviews" element={<Reviews />} />
+        <Route path="/scheduleTimings" element={<ScheduleTimings />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/socialMedia" element={<SocialMedia />} />
+        <Route path="/termCondition" element={<TermCondition />} />
+        <Route path="/videoCall" element={<VideoCall />} />
+        <Route path="/voiceCall" element={<VoiceCall />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/doctorRegister" element={<DoctorRegister />} />
+        <Route path="/Invoice" element={<Invoice />} />
+        <Route path="/patientProfile" element={<PatientProfile />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/addPrescription" element={<AddPrescription />} />
+        <Route path="/addBilling" element={<AddBilling />} />
+        <Route path="/doctorProfileSettings" element={<DoctorProfileSettings />} />
+        <Route path="/doctorDashboard" element={<DoctorDashboard />} />
+        <Route path="/UserDashboard" element={<UserDashboard />} />
+        <Route path="/bookingSuccess" element={<BookingSuccess />} />
+        <Route path="/invoiceView" element={<InvoiceView />} />
+        <Route path="/ChangePassword" element={<ChangePassword />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+        <Route path="/Appointments" element={<Appointments />} />
+
+        {/* Các route cho admin */}
+        <Route path="/admin/*" element={<AdminLayout />}>
+          <Route path="AdminDashboard" element={<AdminDashboard />} />
+          <Route path="specialities" element={<Specialities />} />
+          <Route path="DoctorList" element={<DoctorList />} />
+          <Route path="PatientList" element={<PatientList />} />
+          <Route path="TransactionsList" element={<TransactionsList />} />
+          <Route path="appointment" element={<Appointment />} />
+        </Route>
+      </Routes>
+
+      {/* Chỉ hiển thị Footer nếu không phải route admin */}
+      {!isAdminRoute && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -43,50 +116,7 @@ function App() {
       <SnackbarProvider maxSnack={3}>
         <QueryClientProvider client={new QueryClient()}>
           <BrowserRouter>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/doctorProfile" element={<DoctorProfile />} />
-              <Route
-                path="/doctorAppointment"
-                element={<doctorAppointment />}
-              />
-              <Route path="/patientProfile" element={<PatientProfile />} />
-              <Route path="/patientDashboard" element={<patientDashboard />} />
-              <Route path="/privacyPolicy" element={<privacyPolicy />} />
-              <Route path="/profileSettings" element={<ProfileSettings />} />
-              <Route path="/Reviews" element={<Reviews />} />
-              <Route path="/scheduleTimings" element={<ScheduleTimings />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/socialMedia" element={<socialMedia />} />
-              <Route path="/termCondition" element={<TermCondition />} />
-              <Route path="/videoCall" element={<VideoCall />} />
-              <Route path="/voiceCall" element={<VoiceCall />} />
-              <Route path="/favourites" element={<Favourites />} />
-              <Route path="/booking" element={<Booking />} />
-              <Route path="/chat" element={<Chat />} />
-              <Route path="/doctorRegister" element={<DoctorRegister />} />
-              <Route path="/Invoice" element={<Invoice />} />
-              <Route path="/patientProfile" element={<PatientProfile />} />
-              <Route path="/Checkout" element={<Checkout />} />
-              <Route path="/addPrescription" element={<AddPrescription />} />
-              <Route path="/addBilling" element={<AddBilling />} />
-              <Route
-                path="/doctorProfileSettings"
-                element={<DoctorProfileSettings />}
-              />
-              <Route path="/doctorDashboard" element={<DoctorDashboard />} />
-              <Route path="/UserDashboard" element={<UserDashboard />} />
-              <Route path="/bookingSuccess" element={<BookingSuccess />} />
-              <Route path="/invoiceView" element={<InvoiceView />} />
-              <Route path="/ChangePassword" element={<ChangePassword />} />
-              <Route path="/ForgotPassword" element={<ForgotPassword />} />
-              <Route path="/Appointments" element={<Appointments />} />
-            </Routes>
-            <Footer />
+            <Layout />
           </BrowserRouter>
         </QueryClientProvider>
       </SnackbarProvider>
